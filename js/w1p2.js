96cffcfdacad1440b058c593d7a18d25bf2b1e33
Brad Thomason <brad@badfeather.com>
2022-01-12 19:47:21 -0500

    added w1p2

--- js/w1p2.js
+++ js/w1p2.js
@@ -1,4 +1,4 @@
-let convertMetricWeight = (function () {
+let MetricWeight = (function () {
 	let pg = 1,
 		ng = pg * 1000,
 		mcg = ng * 1000,
@@ -14,7 +14,7 @@ let convertMetricWeight = (function () {
 		Gt = Mt * 1000,
 		fromPg = {pg, ng, mcg, mg, cg, dg, g, dag, hg, kg, t, Mt, Gt};
 
-	let uNames = {
+	let units = {
 		pg: 'picogram',
 		ng: 'nanogram',
 		mcg: 'microgram',
@@ -34,20 +34,20 @@ let convertMetricWeight = (function () {
 	 * Helper function - Convert number in unit to picograms
 	 * @param {Number} n The input weight
 	 * @param {string} u The input unit abbreviation
-	 * @see uNames object keys for available units for u
+	 * @see units object keys for available units for u
 	 */
 	function n2pg (n, u) {
-		return parseFloat(n) * fromPg[u];
+		return n * fromPg[u];
 	}
 
 	/**
 	 * Helper Function - Convert number in picograms
 	 * @param {Number} n The input weight in picograms
 	 * @param {string} u The unit abbreviation to convert to
-	 * @see uNames object keys for available units for u
+	 * @see units object keys for available units for u
 	 */
 	function pg2u (n, u) {
-		return parseFloat(n) / fromPg[u];
+		return n / fromPg[u];
 	}
 
 	/**
@@ -55,25 +55,44 @@ let convertMetricWeight = (function () {
 	 * @param {Number} n The input weight
 	 * @param {string} inU The unit abbreviation for the input weight
 	 * @param {string} outU The unit abbreviation for the output weight
-	 * @see uNames object keys for available units for inU and outU
-	 * param {boolean} addUnit Whether or not to show the unit. Defaults to true.
-	 * param {boolean} abbr Whether or not to abbreviate the unit. Defaults to true.
 	 */
-	function u2u (n, inU, outU, showUnit = 1, abbr = 1) {
-		let val = pg2u(n2pg(n, inU), outU);
-		if (!showUnit) return val;
-		if (abbr) return val + ' ' + outU;
-		return val + ' ' + uNames[outU] + (val === 1 ? '' : 's');
+	function convertTo (n, inU, outU) {
+		return pg2u(n2pg(n, inU), outU);
 	}
 
-	// Export public methods
-	return {u2u};
+	function Constructor (weight, unit) {
+		this.weight = parseFloat(weight);
+		this.unit = units.hasOwnProperty(unit) ? unit : 'g';
+	}
+
+	/**
+	 * Convert number in unit to picograms
+	 * @param {string} unit The unit abbreviation to convert to
+	 * @see units object keys for available units
+	 */
+	Constructor.prototype.convertTo = function (unit) {
+		if (!units.hasOwnProperty(unit)) throw 'Not a valid unit.';
+		this.weight = convertTo(this.weight, this.unit, unit);
+		this.unit = unit;
+	}
+
+	/**
+	 * Return formatted string including the weight and the unit
+	 * @param {boolean} abbr Whether to display the abbreviated or full unit name
+	 */
+	Constructor.prototype.format = function (abbr = 1) {
+		let unit = abbr ? this.unit : units[this.unit];
+		return this.weight + ' ' + unit;
+	}
+
+	// Export constructor object
+	return Constructor;
 })();
 
 // TEST FORM
 // -----------------------------------------------
 let form = document.getElementById('form'),
-	mass = document.getElementById('mass'),
+	weight = document.getElementById('weight'),
 	fromUnits = document.getElementById('from-units'),
 	toUnits = document.getElementById('to-units'),
 	showUnits = document.getElementById('show-units'),
@@ -82,11 +101,22 @@ let form = document.getElementById('form'),
 
 form.addEventListener('submit', function(event) {
 	event.preventDefault();
-	if (!form || ! mass.value) return;
-	let n = mass.value,
+	if (!form || ! weight.value) return;
+	let n = weight.value,
 		inU = fromUnits.value,
 		outU = toUnits.value,
-		i = false;
+		newWeight = new MetricWeight(n, inU),
+		resultText,
+		unit;
+	console.log(weight);
+	newWeight.convertTo(outU);
 
-	result.innerHTML = convertMetricWeight.u2u(n, inU, outU, showUnits.checked, abbrUnits.checked);
+	if (showUnits.checked) {
+		let abbr = abbrUnits.checked ? 1 : 0;
+		resultText = newWeight.format(abbr);
+	} else {
+		resultText = newWeight['weight'];
+	}
+
+	result.innerHTML = resultText;
 });
